4e6131ebb3d6109ed4cfc38e3daca8d35d1a9113
Griff Hall <griff@Griffs-MBP.fios-router.home>
2017-07-28 17:26:39 -0400

    changed from noto homebuilder email address

--- signup.php
+++ signup.php
@@ -2,9 +2,9 @@
 
 $email = $_POST['email'];
 $formcontent="New user: $email";
-$recipient = "matt@notohomebuilders.com";
+$recipient = "info@piddly.com";
 $subject = "A new user signed up for Piddly";
-$mailheader = "From: $email \r\n";
+$mailheader = "From: info@piddly.com \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 
 
